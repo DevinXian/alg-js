27a4055b34ba3cbf4c8a1deb966e64d21e8223da
luofeng <devinxian@gmail.com>
2021-11-07 16:49:32 +0800

    feat: binary search comment

--- binary-search/01.js
+++ binary-search/01.js
@@ -1,4 +1,4 @@
-// 假设list增序
+// 假设list增序 - 无法去除重复
 exports.bs = function bs(list, target) {
   if (!list.length) return -1;
 
@@ -22,7 +22,7 @@ exports.bs = function bs(list, target) {
   return -1;
 }
 
-// 向左边界收敛
+// 向左边收敛 - 可去重复,取最左边第一个
 exports.bs_left_boundary = function (list, target) {
   if (!list.length) return -1;
   let left = 0;
@@ -40,8 +40,38 @@ exports.bs_left_boundary = function (list, target) {
     }
   }
 
-  if (left >= list.length) return -1; // 全部比target小，left + 1 溢出
+  if (left >= list.length) return -1; // 全部比target小，left一直向右移动到溢出
   if (list[left] !== target) return -1; // 全部比target大，left = 0; left总是在mid右侧，故不会小于0
 
-  return left
+  return left // 别搞错了，当right在==target时候总会-1，向左收敛，返回left
 }
+
+// 向右边收敛 - 可去重复，取最右边第一个
+exports.bs_right_boundary = function (list, target) {
+  if (!list.length) return -1;
+  let left = 0;
+  let right = list.length - 1;
+
+  while (left <= right) {
+    let mid = Math.ceil(left + (right - left) / 2)
+
+    if (target < list[mid]) { // 缩小范围到左半区
+      right = mid - 1;
+    } else if (target > list[mid]) { // 缩小范围到右半区
+      left = mid + 1;
+    } else {
+      left = mid + 1; // 找到后，向右边收缩边界
+    }
+  }
+
+  // 全部比target大,右边界(right)一直往左移动, right==left==0时，right变为-1,循环中止
+  if (right < 0) return -1;
+  // 全部比target小，right = list.length - 1不动，left不断向右移动,但总是不会大于right
+  if (list[right] !== target) return -1;
+
+  return right // 别搞错了
+}
+
+// console.log(exports.bs_right_boundary([1, 2, 3, 4, 5], 6))
+// console.log(exports.bs_right_boundary([1, 2, 3, 4, 5], 0))
+// console.log(exports.bs_right_boundary([1, 2, 3, 4, 5], 4))
--- binary-search/01.test.js
+++ binary-search/01.test.js
@@ -1,4 +1,4 @@
-const { bs, bs_left_boundary } = require('./01')
+const { bs, bs_left_boundary, bs_right_boundary } = require('./01')
 
 function run(bs) {
   it('empty should return -1', () => {
@@ -32,6 +32,16 @@ describe('binary search', () => {
     it('should find the target when ele is duplicated', () => {
       const list = [1, 2, 5, 5, 5]
       const index = list.findIndex(i => i === 5); // 3
+      expect(bs_left_boundary(list, 5)).toBe(index)
+    })
+  })
+
+  describe('binary search to right boundary', () => {
+    run(bs_right_boundary)
+    // basic alg can't handle this case
+    it('should find the target when ele is duplicated', () => {
+      const list = [1, 2, 3, 5, 5]
+      const index = list.reverse().findIndex(i => i === 5); // 0
       expect(bs_left_boundary(list, 5)).toBe(index)
     })
   })
