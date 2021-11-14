16cc86a046a1c4b1d4e867afe699e402b8d5df57
maxleisun <maxleisun@tencent.com>
2021-11-15 00:32:07 +0800

    feat(lis): 最长增长子序列分堆解法

--- longest-increase-subsequence/index.js
+++ longest-increase-subsequence/index.js
@@ -1,3 +1,6 @@
+/**
+ * 算法时间复杂度O(n^2)
+ */
 exports.dp = function (list) {
   if (!list || !list.length) return 0;
 
@@ -24,4 +27,49 @@ exports.dp = function (list) {
 
   // 寻找最大值 - 可能有多段或者交叉增长子序列，跟i无关，所以需要遍历
   return dp.reduce((prev, curr) => Math.max(prev, curr), dp[0]);
+};
+
+/**
+ * 这个算法纯属书中记载，确实想不到；尝试解析一二
+ * 算法过程类似纸牌堆叠成若干堆，规则如下：
+ * 1. 小压大
+ * 2. 当前牌不满足条件1，则新起牌堆
+ * 3. 多个牌堆可选，放左边；因为要满足有序顶部；
+ *    举例：顶部有3和4（注意，因条件1，不可能是3和4），则2放在左侧，可有[2,4]排列，反过来则不成立
+ * 4. 则最后堆数即为结果
+ *    解析：1跟2保证了每个牌堆至少有一个元素是大于上一个牌堆某元素（典型为最上面牌)，则堆数就是最长递增子序列
+ */
+exports.piles = function (list) {
+  if (!list || !list.length) return 0;
+
+  let result = 0; // 堆数
+  const top = []; // 保存每个堆顶部的元素
+
+  for (let i = 0; i < list.length; i++) {
+    const ele = list[i];
+
+    // 找堆-往左边放-找左侧边界的二分查找法
+    let left = 0;
+    let right = result - 1;
+
+    while (left <= right) {
+      const mid = left + Math.floor((right - left) / 2);
+
+      if (top[mid] > ele) {
+        right = mid - 1;
+      } else if (top[mid] < ele) {
+        left = mid + 1;
+      } else {
+        right = mid - 1; // 相等则向左侧收敛边界
+      }
+    }
+
+    // 没找到牌堆，left都溢出了，则新起一堆
+    if (left === result) result += 1;
+
+    // 放堆顶元素 --- ps：进一步可以存数组，方便直观查看LIS
+    top[left] = ele;
+  }
+
+  return result;
 };
--- longest-increase-subsequence/index.test.js
+++ longest-increase-subsequence/index.test.js
@@ -1,23 +1,31 @@
-const { dp } = require('./index');
+const { dp, piles } = require('./index');
 
-describe('lis: longest increase subsequence - dp', () => {
+function run(method) {
   test('empty list', () => {
-    expect(dp([])).toBe(0);
+    expect(method([])).toBe(0);
   });
   test('increasing list', () => {
     const list = [0, 1, 2, 3, 4, 5];
-    expect(dp(list)).toBe(list.length);
+    expect(method(list)).toBe(list.length);
   });
   test('decreasing list', () => {
     const list = [5, 4, 3, 2, 1, 0];
-    expect(dp(list)).toBe(1);
+    expect(method(list)).toBe(1);
   });
   test('ordonary list 1', () => {
     const list = [1, 0, 4, 5, 1, 8, 2, 3, 4];
-    expect(dp(list)).toBe(5);
+    expect(method(list)).toBe(5);
   });
   test('ordonary list 2', () => {
     const list = [1, 10, 40, 5, 1, 8, 20, 30];
-    expect(dp(list)).toBe(5);
+    expect(method(list)).toBe(5);
   });
+}
+
+describe('LIS - dp', () => {
+  run(dp);
+});
+
+describe('LIS - poker piles', () => {
+  run(piles);
 });
